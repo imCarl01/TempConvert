f0c4a203ec195a59398219b3ba4ff0007117a119
imCarl01 <imcarl0.uc@gmail.com>
2024-06-09 03:04:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const textbox = document.getElementById("textbox");
+const itFarenheit = document.getElementById("itFarenheit");
+const itCelius = document.getElementById("itCelius");
+// const submitbtn = document.getElementById("submitbtn");
+const result = document.getElementById("result");
+let temp;
+
+function convert(){
+    if(itFarenheit.checked){
+        temp = Number(textbox.value);
+        temp = 32 + (9 / 5 * temp);
+        result.textContent = temp.toFixed(1); 
+    }
+    else if(itCelius.checked){
+        temp = Number(textbox.value);
+        temp = ( temp - 32) * 5 / 9;
+        result.textContent = temp;
+    }
+    else{
+        result.textContent = 'Select a Unit'
+    }
+}
+
