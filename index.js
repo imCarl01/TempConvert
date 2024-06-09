1dc3551ed887d388937c222e5183c293925680dc
Gift Jacksun <giftjacksun@gmail.com>
2024-06-09 18:02:42 +0100

    fixed the bug

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const textbox = document.getElementById("textbox");
 const itFarenheit = document.getElementById("itFarenheit");
 const itCelius = document.getElementById("itCelius");
-// const submitbtn = document.getElementById("submitbtn");
 const result = document.getElementById("result");
+const form = document.querySelector("form")
+
 let temp;
 
-function convert(){
+form.addEventListener("submit", convert)
+
+function convert(e) {
+    e.preventDefault(); //prevent the default behaviour
     if(itFarenheit.checked){
-        temp = Number(textbox.value);
+        temp = textbox.value;
         temp = 32 + (9 / 5 * temp);
         result.textContent = temp.toFixed(1); 
     }
     else if(itCelius.checked){
-        temp = Number(textbox.value);
+        temp = textbox.value;
         temp = ( temp - 32) * 5 / 9;
         result.textContent = temp;
     }
-    else{
+    else if(!itCelius.checked && !itFarenheit.checked){
         result.textContent = 'Select a Unit'
     }
 }
+
+
+
 
